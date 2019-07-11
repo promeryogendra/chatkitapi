92df11f03fbaea513ad049836f660c7caf4f5c98
promeryogendra <yogendra.rgu@gmail.com>
2019-07-11 17:11:39 +0530

    first commit

---
+++ common/models/customer.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var app = require('../../server/server.js');
+
+let data_config = require('../../server/data/email-host-config.json');
+let path = require('path');
+
+let senderAddress = data_config.hostmail;
+
+//Testing code
+// function testing(context) {
+//   let Customer = app.models.Customer;
+//   Customer.findOne({where: {username : "yogendra"}}, function(err , user){
+//     if(err){ console.log(error);return err;}
+//     console.log("---",user);
+//     return user;
+//   })
+// }
+
+module.exports = function(Customer) {
+
+  //     Customer.greet = async function(msg) {
+  //       return 'Greetings... ' + msg;
+  //   }
+
+  //   Customer.remoteMethod('greet', {
+  //         accepts: {arg: 'msg', type: 'string'},
+  //         returns: {arg: 'greeting', type: 'string'}
+  //   });
+  // Customer.testing = function(msg, cb) {
+  //   cb(null, 'Greetings....' + msg);
+  // };
+  // Customer.remoteMethod('testing', {
+  //   accepts: {
+  //     arg: 'msg',
+  //     type: 'string',
+  //   },
+  //   returns: {
+  //     arg: 'greeting',
+  //     type: 'string',
+  //   },
+  //   http: {
+  //     path: '/testing',
+  //     verb: 'get',
+  //   },
+  // });
+  // Customer.beforeRemote('testing', (context, modelInstance, next) => {
+  //   // var data = sent(context);
+  //   var data = testing(context);
+  //   setTimeout(() => {
+  //     if (resData.length > 0) {
+  //       context.res.status(200);
+  //       context.res.send(resData);
+  //       next(resData);
+  //     }else {
+  //       context.res.status(204);
+  //       next();
+  //     }
+  //   }, 1000);
+  // });
+  //Create error
+  function createError(status,msg,code) {
+    let error = new Error();
+    error.status=status;
+    error.statusCode = status;
+    error.stack = status;
+		error.message = msg;
+		error.code = code;
+    return error;
+  }
+  //find the user has the data or not
+  Customer.beforeRemote('create', function(context, modelInstance, next) {
+    let username = context.req.body.username;
+    let password = context.req.body.password;
+    let email = context.req.body.email;
+    if(username !=undefined && username.length < 6) {
+			next(createError(400 , "Username length must be atleast 6 chars.","USERNAME_LENGTH"));
+		} else if(password != undefined && password.length < 6) {
+			next(createError(400 , "Password length must be atleast 6 chars.","PASSWORD_LENGTH"));
+		} else {
+      context.req.body = {
+        "username" : username,
+        "email" : email,
+        "password": password
+      }
+      next();
+    }
+  });
+  //After remote hook for create method
+  Customer.afterRemote('create', function(context, user, next) {
+    var options = {
+      type: 'email',
+      to: user.email,
+      from: senderAddress,
+			host: data_config.hostaddress,
+			port: data_config.port,
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: user
+    };
+
+    user.verify(options, function(err, response) {
+      if (err) {
+        Customer.deleteById(user.id);
+        return next(err);
+      }
+			next();
+    });
+  });
+  //AfterremoteError
+  Customer.afterRemoteError('create', function(context, next) {
+    // console.log(context,context.error,context.error.status,context.error.code);
+    next();
+  });
+
+  //Login remote method
+  Customer.afterRemote('login', function (ctx, result, next) {
+    Customer.findOne({where : {id:result.userId}} , (err , user) => {
+      if(err) {
+        next(createError(500,"Server error.","SERVER_ERROR"));
+      } else if( !user ) {
+        next(createError(200 , "User not found" , "USER_NOT_FOUND"));
+      } else {
+        ctx.result = {
+          id: result.id,
+          userId: result.userId,
+          username: user.username,
+          email: user.email
+        };
+        next();
+      }
+    })
+  });
+};
---
+++ server/boot/customer-methods.js
@@ -0,0 +1,97 @@
+module.exports = function (app) {
+  var router = app.loopback.Router();
+
+  function createResponse(res, status, message, name, code) {
+    res.statusCode = status;
+    res.status = status;
+    res.message = message;
+    res.name = name;
+    res.code = code;
+  }
+  //Check the user is authenticated or not
+  app.get('/api/verifyUser' , (req , res) => {
+    let tokenId = req.body.tokenId;
+    let userId = req.body.userId;
+    if(tokenId  && userId && tokenId !== '' && userId !== '')
+    {
+      var Customer = app.models.Customer;
+      var AccessToken = app.models.AccessToken;
+      AccessToken.resolve(tokenId, (err, accessToken) => {
+        if (err) {
+          createResponse(res , 500 , "Server having issue..." , "Error" , 500)
+          res.send("Server issue...");
+        } else if ( ! accessToken) {
+          createResponse(res , 204 , "No Content Found..." , 'Success' , 204);
+          res.send(false);
+        } else {
+          Customer.find({where: {id:accessToken.userId}}, function (err, users) {
+
+            if (err) {
+              createResponse(res , 500 , "Server having issue..." , "Error" , 500)
+              res.send("Server issue...");
+            }else if (!users || users === [] ) {
+              createResponse(res , 204 , "Not Authenticated user..." , 'Success' , 204);
+              res.send(false);
+            } else {
+              if(users[0].id+"" === userId) {
+                createResponse(res , 200 , "Authenticated user..." , "Success" , 200);
+                res.send(true);
+              } else {
+                createResponse(res , 204 , "Not Authenticated user..." , "Success" , 204);
+                res.send(false);
+              }
+            }
+          });
+        }
+      });
+    } else {
+      createResponse(res , 400 , "parameters missing.." , "Error" , 400)
+      res.send("Data required to perform Action...");
+    }
+  });
+  //Check user Email or Username exists or not
+  app.get('/api/check' , (req , res) => {
+    let checkName = req.body.checkName;
+    var Customer = app.models.Customer;
+    if(checkName != undefined && checkName !== '') {
+      let username ;
+      if(checkName === 'username') username = req.body.username;
+      let email;
+      if(checkName === 'email')  email = req.body.email;
+      let filters;
+      let letsCheck = false;
+      if(checkName === 'email' && email !=undefined && email !== '') {
+        filters = {
+          where : {  "email" : email}
+        }
+        letsCheck = true;
+      } else if( checkName === 'username' && username != undefined && username !== '') {
+        filters = {
+          where : { "username" : username}
+        }
+        letsCheck = true;
+      }
+      if(letsCheck) {
+        Customer.findOne(filters , (err , user) => {
+           if (err) {
+            createResponse(res , 500 , "Server having issue..." , "Error" , 500)
+            res.send("Server issue...");
+          } else if (!user) {
+            createResponse(res , 204 , checkName+" not exists." , "Success" , 204);
+            res.send(false);
+          } else {
+            createResponse(res , 200 , checkName+" Exists..." , "Success" , 200);
+            res.send(true);
+          }
+        })
+      } else {
+        createResponse(res , 400 , "parameters missing.." , "Error" , 400)
+        res.send("Data required to perform Action...");
+      }
+    } else {
+      createResponse(res , 400 , "parameters missing.." , "Error" , 400)
+      res.send("Data required to perform Action...");
+    }
+  });
+  app.use(router);
+}
